18a18616cb8ab3ec279c20a85b518d20fdcba418
Davidm8624 <davidm8624@gmail.com>
2023-06-03 17:16:53 -0700

    data collection working

--- add_data.rb
+++ add_data.rb
@@ -5,16 +5,17 @@ end
 person1 = Person.new()
 person1.name = gets.to_s #this isnt nessisary as gets automaticly accepts strigs
 person1.age = gets.to_i
-sex_buffer = gets
+
+
 
 def get_sex_type (sex)
   get_sex = ""
 
-case sex #whaaaaaaaaaaat sex code?!?!?!?    :O
-when "male"
+case sex
+when "m"
   get_sex = "male"
   puts "got male!"
-when "female"
+when "f"
   get_sex = "female"
   puts "got female!"
 else
@@ -25,10 +26,34 @@ end
 return get_sex
 end
 
-person1.sex = get_sex_type(sex_buffer)
+person1.sex = get_sex_type(gets.chomp)
+person1.fav_color = gets.chomp
 
 
+def credit(score)
+  if (score < 575)
+    return "poor"
+  elsif (score > 576 && score < 669)
+    return "fair"
+  elsif (score > 670 && score < 719)
+    return "good"
+  elsif (score > 720 && score < 799)
+    return "vary good"
+  elsif (score > 800)
+    return "excelent"
+  else
+    return "no credit"
+end
+end
+
+person1.credit_score = credit(gets.to_i)
+
+#console output
 puts "OUTPUT:"
 puts person1.name
 puts person1.age
 puts person1.sex
+puts person1.fav_color
+puts person1.credit_score
+
+File.write("long-term-storage.txt", person1, mode: "a")
